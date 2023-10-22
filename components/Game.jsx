5514e48f48f96130b9c5396cc7d63686ad706e7f
Nathan <nathan.savari.contact@gmail.com>
2023-10-22 22:38:18 +0200

    try fix iphone

--- components/Game.jsx
+++ components/Game.jsx
@@ -115,8 +115,8 @@ export default function Game({ roomId }) {
               ref={ref}
               className={styles.input}
               id="input"
-              type="tel"
-              pattern="[0-9]*"
+              type="number"
+              pattern="\d*"
               name="response"
               onChange={(event) => setResponse(event.target.value)}
               required
