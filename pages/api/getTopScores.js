79cc5b2e544af0f9059ae8d929b162d32ff117e6
Nathan <nathan.savari.contact@gmail.com>
2023-10-28 21:18:30 +0200

    limit to 15

--- pages/api/getTopScores.js
+++ pages/api/getTopScores.js
@@ -29,7 +29,7 @@ export default async function handler(req, res) {
         },
       },
       { $sort: { score: -1 } },
-      { $limit: 10 },
+      { $limit: 15 },
     ];
 
     const topScores = await collection.aggregate(pipeline).toArray();
